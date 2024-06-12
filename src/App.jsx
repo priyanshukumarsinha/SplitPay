825ec4964f5a648965d37f46df937479d8a2ce81
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-06-12 09:07:05 +0530

    tailwind - colors - definition

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      {/* <div className='w-full h-full bg-[]'>
+
+      </div> */}
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        background : '#222831',
+        backgroundShade : "rgba(39, 49, 69, 0.42)",
+        textColor : "#fff",
+        textDim : "#9A9A9A",
+        primary : "#F96D00",
+        primaryShade : "#E57B28",
+        secondary : "#393E46",
+        secondaryShade : "#4B5B75",
+        accent : "#F2F2F2",
+        errorMsg : "#BA4848",
+        successMsg : "#57C248",
+      }
+    },
   },
   plugins: [],
 }
